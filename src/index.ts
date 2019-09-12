00ced25370e565cc4237b01435bb81dfbe9e6bbb
Sandrino Di Mattia <contact@sandrino.be>
2019-09-12 21:46:44 +0200

    Call webpack if the plugin is wrapped by other plugins

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,21 @@ import { ISignInWithAuth0 } from './instance';
 export function useAuth0(settings: IAuth0Settings): ISignInWithAuth0 {
   const isBrowser = typeof window !== 'undefined' || (process as any).browser;
   if (isBrowser) {
-    return require('./instance.browser').default(settings); // eslint-disable-line
+    return require('./instance.browser').default(settings);
   }
 
   return require('./instance.node').default(settings);
 };
 
-export function withAuth0(nextConfig: any = {}): any {
+export function withAuth0(nextConfig: any = {}, options: any = {}): any {
   return Object.assign({}, nextConfig, {
     webpack(config: any, { isServer }: { isServer: boolean }): any {
       if (!isServer) {
         config.externals = ['openid-client'];
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
       }
 
       return config;
