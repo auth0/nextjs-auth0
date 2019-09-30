cd7b19115bf0849a30e2b5675cf240008b0095a7
JJ Kasper <jj@jjsweb.site>
2019-09-30 19:44:15 +0200

    Add deprecation for useAuth0

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import IAuth0Settings from './settings';
 import { ISignInWithAuth0 } from './instance';
-import Instance from './instance.browser'
+import Instance from './instance.browser';
 
 // @ts-ignore un-used settings
 export function initAuth0(settings: IAuth0Settings): ISignInWithAuth0 {
--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,8 @@ export function initAuth0(settings: IAuth0Settings): ISignInWithAuth0 {
 
   return require('./instance.node').default(settings);
 }
+
+/**
+ * @deprecated useAuth0 has been deprecated in favor of initAuth0
+ */
+export const useAuth0 = initAuth0;
