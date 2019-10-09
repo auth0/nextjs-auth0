c8f897fd6b4e5dd583e963ce54a99894aaf23070
Sandrino Di Mattia <contact@sandrino.be>
2019-10-09 14:00:15 +0200

    Added logic to require authentication in an API

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,11 +3,13 @@ import LogoutHandler from './logout';
 import CallbackHandler from './callback';
 import ProfileHandler from './profile';
 import SessionHandler from './session';
+import RequireAuthentication from './require-authentication';
 
 export default {
   CallbackHandler,
   LoginHandler,
   LogoutHandler,
   ProfileHandler,
-  SessionHandler
+  SessionHandler,
+  RequireAuthentication
 };
