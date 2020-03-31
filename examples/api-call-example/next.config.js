e724719f7be191e122ba664b16d07926aa18d980
Sandrino Di Mattia <contact@sandrino.be>
2020-03-31 03:02:54 +0200

    Minor updates to samples

--- examples/api-call-example/next.config.js
+++ examples/api-call-example/next.config.js
@@ -9,7 +9,7 @@ module.exports = {
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_SCOPE: 'openid profile read:shows offline_access',
     API_AUDIENCE: 'https://api/tv-shows',
-    API_BASE_URL: 'http://localhost:3001',
+    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3001',
     REDIRECT_URI: process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
     POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/',
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
