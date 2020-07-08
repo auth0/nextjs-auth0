f292729e0d71f55afba0f32218a3f7d967ed6c51
Egor <egor@nidratech.com>
2020-07-08 06:20:42 -0700

    allow the logout returnTo URL to be configurable at request-time (#125)
    Co-authored-by: Egor <philippovegor@gmail.com>

--- src/handlers/logout.ts
+++ src/handlers/logout.ts
@@ -6,12 +6,12 @@ import { IOidcClientFactory } from '../utils/oidc-client';
 import CookieSessionStoreSettings from '../session/cookie-store/settings';
 import { ISessionStore } from '../session/store';
 
-function createLogoutUrl(settings: IAuth0Settings): string {
-  return (
-    `https://${settings.domain}/v2/logout?` +
-    `client_id=${settings.clientId}` +
-    `&returnTo=${encodeURIComponent(settings.postLogoutRedirectUri)}`
-  );
+export interface LogoutOptions {
+  returnTo?: string;
+}
+
+function createLogoutUrl(settings: IAuth0Settings, returnToUrl: string): string {
+  return `https://${settings.domain}/v2/logout?client_id=${settings.clientId}&returnTo=${returnToUrl}`;
 }
 
 export default function logoutHandler(
@@ -20,7 +20,7 @@ export default function logoutHandler(
   clientProvider: IOidcClientFactory,
   store: ISessionStore
 ) {
-  return async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  return async (req: NextApiRequest, res: NextApiResponse, options?: LogoutOptions): Promise<void> => {
     if (!req) {
       throw new Error('Request is not available');
     }
@@ -31,17 +31,18 @@ export default function logoutHandler(
 
     const session = await store.read(req);
     let endSessionUrl;
+    const returnToUrl = encodeURIComponent(options?.returnTo || settings.postLogoutRedirectUri);
 
     try {
       const client = await clientProvider();
       endSessionUrl = client.endSessionUrl({
         id_token_hint: session ? session.idToken : undefined,
-        post_logout_redirect_uri: encodeURIComponent(settings.postLogoutRedirectUri)
+        post_logout_redirect_uri: returnToUrl
       });
     } catch (err) {
       if (/end_session_endpoint must be configured/.exec(err)) {
         // Use default url if end_session_endpoint is not configured
-        endSessionUrl = createLogoutUrl(settings);
+        endSessionUrl = createLogoutUrl(settings, returnToUrl);
       } else {
         throw err;
       }
