88959971958e5c6ed5bd874828c97363d2224f74
Sandrino Di Mattia <contact@sandrino.be>
2020-07-13 12:35:46 +0200

    Improve redirect validation for double forward slash urls

--- src/utils/url-helpers.ts
+++ src/utils/url-helpers.ts
@@ -1,6 +1,16 @@
-export default function isRelative(url: string): boolean {
+/**
+ * Helper which tests if a URL can safely be redirected to. Requires the URL to be relative.
+ * @param url
+ */
+export default function isSafeRedirect(url: string): boolean {
   if (typeof url !== 'string') {
     throw new TypeError(`Invalid url: ${url}`);
   }
+
+  // Prevent open redirects using the //foo.com format (double forward slash).
+  if (/\/\//.test(url)) {
+    return false;
+  }
+
   return !/^[a-zA-Z][a-zA-Z\d+\-.]*:/.test(url);
 }
