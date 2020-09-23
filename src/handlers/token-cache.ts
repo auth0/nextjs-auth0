1959d0f8127ac21574949e140a4d1c1712efb0b7
Brendan Quinn <bquinn@sensei.com>
2020-09-23 06:43:23 -0700

    Less specific tokenCacheHandler return type
    The envocation of SessionTokenCache is unecessarily restricted to taking a NextApiRequest and an NextApiResponse.
    
    It can be less specific and take an IncomingMessage and ServerResponse.
    
    In doing so the token cache handler can be utilized in Next.js's `getServerSideProps` without type errors.

--- src/handlers/token-cache.ts
+++ src/handlers/token-cache.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { IncomingMessage, ServerResponse } from 'http';
 
 import { ISessionStore } from '../session/store';
 import { ITokenCache } from '../tokens/token-cache';
@@ -6,7 +6,7 @@ import { IOidcClientFactory } from '../utils/oidc-client';
 import SessionTokenCache from '../tokens/session-token-cache';
 
 export default function tokenCacheHandler(clientProvider: IOidcClientFactory, sessionStore: ISessionStore) {
-  return (req: NextApiRequest, res: NextApiResponse): ITokenCache => {
+  return (req: IncomingMessage, res: ServerResponse): ITokenCache => {
     if (!req) {
       throw new Error('Request is not available');
     }
