c66a98614396e35eacaa8298d6582610b22ed13e
Rita Zerrizuela <zeta@widcket.com>
2020-10-29 21:23:05 -0300

    Export hook, UserProvider and UserProfile

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 import { ConfigParameters } from './auth0-session';
 import { ISignInWithAuth0 } from './instance';
 
+export { useUser, UserProfile, UserProvider } from './hooks/use-user';
+
 export function initAuth0(settings: ConfigParameters): ISignInWithAuth0 {
   const isBrowser = typeof window !== 'undefined' || (process as any).browser;
   if (isBrowser) {
