245d7ccfc5704cfb3c3e198f6c3b31e565d0d15b
adamjmcgrath <adam.mcgrath@auth0.com>
2020-12-18 15:41:10 +0000

    add missing cov

--- tests/handlers/profile.test.ts
+++ tests/handlers/profile.test.ts
@@ -1,8 +1,11 @@
+import nock from 'nock';
 import { withoutApi } from '../fixtures/default-settings';
 import { userInfo } from '../fixtures/oidc-nocks';
 import { get } from '../auth0-session/fixtures/helpers';
 import { setup, teardown, login } from '../fixtures/setup';
 import { Session } from '../../src/session';
+import { makeIdToken } from '../auth0-session/fixtures/cert';
+import { AfterCallback } from '../../src/auth0-session/handlers/callback';
 
 describe('profile handler', () => {
   afterEach(teardown);
@@ -22,7 +25,7 @@ describe('profile handler', () => {
   });
 
   test('should throw if re-fetching with no Access Token', async () => {
-    const afterCallback = (_req: any, _res: any, session: Session): Session => {
+    const afterCallback: AfterCallback = (_req, _res, session: Session): Session => {
       delete session.accessToken;
       return session;
     };
@@ -44,5 +47,32 @@ describe('profile handler', () => {
     userInfo(withoutApi, 'eyJz93a...k4laUWw', {});
     const profile2 = await get(baseUrl, '/api/auth/me', { cookieJar });
     expect(profile2).toMatchObject({ foo: 'bar', nickname: '__test_nickname__', sub: '__test_sub__' });
+  });
+
+  test("should refetch the user and fail if it can't get an access token", async () => {
+    const afterCallback: AfterCallback = (_req, _res, session: Session): Session => {
+      session.accessTokenExpiresAt = -60;
+      return session;
+    };
+    const baseUrl = await setup(withoutApi, {
+      profileOptions: { refetch: true },
+      userInfoPayload: { foo: 'bar' },
+      callbackOptions: {
+        afterCallback
+      }
+    });
+    const cookieJar = await login(baseUrl);
+
+    nock(`${withoutApi.issuerBaseURL}`)
+      .post('/oauth/token', `grant_type=refresh_token&refresh_token=GEbRxBN...edjnXbL`)
+      .reply(200, {
+        id_token: makeIdToken({ iss: 'https://acme.auth0.local/' }),
+        token_type: 'Bearer',
+        expires_in: 750,
+        scope: 'read:foo write:foo'
+      });
+    await expect(get(baseUrl, '/api/auth/me', { cookieJar })).rejects.toThrow(
+      'No access token available to refetch the profile'
+    );
   });
 });
