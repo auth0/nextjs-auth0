bdd3ffad1469bc625aaf729591a09eeb70f9cb2c
Rita Zerrizuela <zeta@widcket.com>
2020-12-21 02:53:42 -0300

    Rename test symbols

--- tests/frontend/with-csr-auth-required.test.tsx
+++ tests/frontend/with-csr-auth-required.test.tsx
@@ -19,18 +19,18 @@ describe('with-csr-auth-required', () => {
   it('should block access to a CSR page when not authenticated', async () => {
     (global as any).fetch = fetchUserFailureMock;
     const MyPage = (): JSX.Element => <>Private</>;
-    const WrappedComponent = withCSRAuthRequired(MyPage);
+    const ProtectedPage = withCSRAuthRequired(MyPage);
 
-    render(<WrappedComponent />, { wrapper: withUser() });
+    render(<ProtectedPage />, { wrapper: withUser() });
     await waitFor(() => expect(routerMock.push).toHaveBeenCalledTimes(1));
     await waitFor(() => expect(screen.queryByText('Private')).not.toBeInTheDocument());
   });
 
   it('should allow access to a CSR page when authenticated', async () => {
     const MyPage = (): JSX.Element => <>Private</>;
-    const WrappedComponent = withCSRAuthRequired(MyPage);
+    const ProtectedPage = withCSRAuthRequired(MyPage);
 
-    render(<WrappedComponent />, { wrapper: withUser(user) });
+    render(<ProtectedPage />, { wrapper: withUser(user) });
     await waitFor(() => expect(routerMock.push).not.toHaveBeenCalled());
     await waitFor(() => expect(screen.getByText('Private')).toBeInTheDocument());
   });
@@ -38,17 +38,17 @@ describe('with-csr-auth-required', () => {
   it('should show a custom redirecting message', async () => {
     const MyPage = (): JSX.Element => <>Private</>;
     const OnRedirecting = (): JSX.Element => <>Redirecting</>;
-    const WrappedComponent = withCSRAuthRequired(MyPage, { onRedirecting: OnRedirecting });
+    const ProtectedPage = withCSRAuthRequired(MyPage, { onRedirecting: OnRedirecting });
 
-    render(<WrappedComponent />, { wrapper: withUser() });
+    render(<ProtectedPage />, { wrapper: withUser() });
     await waitFor(() => expect(screen.getByText('Redirecting')).toBeInTheDocument());
   });
 
   it('should accept a returnTo url', async () => {
     const MyPage = (): JSX.Element => <>Private</>;
-    const WrappedComponent = withCSRAuthRequired(MyPage, { returnTo: '/foo' });
+    const ProtectedPage = withCSRAuthRequired(MyPage, { returnTo: '/foo' });
 
-    render(<WrappedComponent />, { wrapper: withUser() });
+    render(<ProtectedPage />, { wrapper: withUser() });
     await waitFor(() => expect(routerMock.push).toHaveBeenCalledWith(expect.stringContaining('?returnTo=/foo')));
   });
 
