7f8033338f74d342afdac919b919cb3e3b6de54d
frederikprijck <frederik.prijck@auth0.com>
2020-12-22 11:40:20 +0100

    Revert incorrect merge

---
+++ examples/kitchen-sync-example/pages/profile.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useUser, withCSRAuthRequired } from '@auth0/nextjs-auth0';
+
+import Layout from '../components/layout';
+
+export default withCSRAuthRequired(function Profile(): React.ReactElement {
+  const { user, loading } = useUser();
+
+  return (
+    <Layout>
+      <h1>Profile</h1>
+
+      {loading && <p>Loading profile...</p>}
+
+      {!loading && user && (
+        <>
+          <p>Profile:</p>
+          <pre id="profile">{JSON.stringify(user, null, 2)}</pre>
+        </>
+      )}
+    </Layout>
+  );
+});
