b5cfd73264fadc2e679837be4306c4f00cf25626
frederikprijck <frederik.prijck@auth0.com>
2020-12-22 17:07:15 +0100

    Move example API to Next

--- examples/kitchen-sink-example/pages/api/shows.ts
+++ examples/kitchen-sink-example/pages/api/shows.ts
@@ -7,14 +7,18 @@ export default withApiAuthRequired(async function shows(req, res) {
       scopes: ['read:shows']
     });
 
-    const response = await fetch('http://localhost:3001/api/my/shows', {
+    const baseURL = process.env.AUTH0_BASE_URL?.indexOf('http') === 0 ? 
+      process.env.AUTH0_BASE_URL : 
+      `https://${process.env.AUTH0_BASE_URL}`;
+
+    const response = await fetch(baseURL + 'api/my/shows', {
       headers: {
         Authorization: `Bearer ${accessToken}`
       }
     });
 
     const shows = await response.json();
-    res.status(200).json(shows);
+    res.status(response.status || 200).json(shows);
   } catch (error) {
     console.error(error);
     res.status(error.status || 500).json({
