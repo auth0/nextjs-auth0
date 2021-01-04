1de2bf703527c3d2301295d05c11c9e949a5c984
frederikprijck <frederik.prijck@auth0.com>
2021-01-04 16:15:46 +0100

    Do not prefix base url when not defined

--- src/config.ts
+++ src/config.ts
@@ -398,7 +398,7 @@ export const getParams = (params?: ConfigParameters): ConfigParameters => {
     AUTH0_COOKIE_SAME_SITE
   } = process.env;
 
-  const baseURL = AUTH0_BASE_URL?.indexOf('http') === 0 ? AUTH0_BASE_URL : `https://${AUTH0_BASE_URL}`;
+  const baseURL = AUTH0_BASE_URL && !AUTH0_BASE_URL.startsWith('http') ? `https://${AUTH0_BASE_URL}` : AUTH0_BASE_URL;
 
   return {
     secret: AUTH0_SECRET,
