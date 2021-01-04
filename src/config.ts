0f87a9386ac373715620dcdb77edc2da6698c9f7
frederikprijck <frederik.prijck@auth0.com>
2021-01-04 14:50:46 +0100

    Prepare application to be deployable with Vercel

--- src/config.ts
+++ src/config.ts
@@ -398,10 +398,12 @@ export const getParams = (params?: ConfigParameters): ConfigParameters => {
     AUTH0_COOKIE_SAME_SITE
   } = process.env;
 
+  const baseURL = AUTH0_BASE_URL?.indexOf('http') === 0 ? AUTH0_BASE_URL : `https://${AUTH0_BASE_URL}`;
+
   return {
     secret: AUTH0_SECRET,
     issuerBaseURL: AUTH0_ISSUER_BASE_URL,
-    baseURL: AUTH0_BASE_URL,
+    baseURL: baseURL,
     clientID: AUTH0_CLIENT_ID,
     clientSecret: AUTH0_CLIENT_SECRET,
     clockTolerance: num(AUTH0_CLOCK_TOLERANCE),
