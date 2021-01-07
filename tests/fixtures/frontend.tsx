6ceda646787bd2c0676ecc0a288522b2645e49de
Rita Zerrizuela <zeta@widcket.com>
2021-01-07 09:40:03 -0300

    Address review feedback

--- tests/fixtures/frontend.tsx
+++ tests/fixtures/frontend.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserProvider, UserProviderProps, UserProfile } from '../../src/frontend';
+import { UserProvider, UserProviderProps, UserProfile } from '../../src';
 
 type FetchUserMock = {
   ok: boolean;
