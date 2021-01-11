b87565df5dee3477b5742262f51f3c6b5c3510e2
Rita Zerrizuela <zeta@widcket.com>
2021-01-11 18:48:06 -0300

    Use data-testid instead of ids

--- examples/kitchen-sink-example/components/header.tsx
+++ examples/kitchen-sink-example/components/header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0';
 
-const Header: React.FunctionComponent = () => {
-  const { user, isLoading } = useUser();
+const Header = (): React.ReactElement => {
+  const { user } = useUser();
 
   return (
     <header>
@@ -24,32 +24,31 @@ const Header: React.FunctionComponent = () => {
               <a>TV Shows</a>
             </Link>
           </li>
-          {!isLoading &&
-            (user ? (
-              <>
-                <li>
-                  <Link href="/profile">
-                    <a>Profile</a>
-                  </Link>
-                </li>{' '}
-                <li>
-                  <a href="/profile-ssr">Profile (SSR)</a>
-                </li>{' '}
-                <li>
-                  <a href="/api/auth/logout" id="logout">
-                    Logout
-                  </a>
-                </li>
-              </>
-            ) : (
-              <>
-                <li>
-                  <a href="/api/auth/login" id="login">
-                    Login
-                  </a>
-                </li>
-              </>
-            ))}
+          {user ? (
+            <>
+              <li>
+                <Link href="/profile">
+                  <a>Profile</a>
+                </Link>
+              </li>{' '}
+              <li>
+                <a href="/profile-ssr">Profile (SSR)</a>
+              </li>{' '}
+              <li>
+                <a href="/api/auth/logout" data-testid="logout">
+                  Logout
+                </a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <a href="/api/auth/login" data-testid="login">
+                  Login
+                </a>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
 
