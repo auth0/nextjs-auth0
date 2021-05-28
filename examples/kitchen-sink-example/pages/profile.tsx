3ee8df71462118fe3f3660566fdbfc1bf37ecc8c
adamjmcgrath <adam.mcgrath@auth0.com>
2021-05-28 15:23:20 +0100

    Add `user` to `withPageAuthRequiredProps` CSR to match SSR

--- examples/kitchen-sink-example/pages/profile.tsx
+++ examples/kitchen-sink-example/pages/profile.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 import Layout from '../components/layout';
 
-export default withPageAuthRequired(function Profile(): React.ReactElement {
-  const { user, error, isLoading } = useUser();
-
+export default withPageAuthRequired(function Profile({ user }) {
   return (
     <Layout>
       <h1>Profile</h1>
-
-      {isLoading && <p>Loading profile...</p>}
-
-      {error && (
-        <>
-          <h4>Error</h4>
-          <pre>{error.message}</pre>
-        </>
-      )}
-
-      {user && (
-        <>
-          <h4>Profile</h4>
-          <pre data-testid="profile">{JSON.stringify(user, null, 2)}</pre>
-        </>
-      )}
+      <h4>Profile</h4>
+      <pre data-testid="profile">{JSON.stringify(user, null, 2)}</pre>
     </Layout>
   );
 });
