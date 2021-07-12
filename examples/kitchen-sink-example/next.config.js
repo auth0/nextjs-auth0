1f5bf56d28b5f3c7eaedc181128291e98e382b2e
Adam Mcgrath <adam.mcgrath@auth0.com>
2021-07-12 13:15:11 +0100

    [SDK-2648] Update Vercel deploy instructions (#440)
    * Update Vercel deploy instructions
    
    * Add env to root
    
    * remove env from root
    
    * deploy
    
    * Add `git` prefix to url
    
    * Update instructions
    
    * Update README

--- examples/kitchen-sink-example/next.config.js
+++ examples/kitchen-sink-example/next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   poweredByHeader: false,
-  target: 'serverless',
-  env: {
-    AUTH0_BASE_URL: process.env.VERCEL_URL || process.env.AUTH0_BASE_URL
-  }
+  target: 'serverless'
 };
