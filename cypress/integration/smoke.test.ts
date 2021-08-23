65a8ce0125a0817f2ace0a5eb0a2221a683a1675
Frederik Prijck <frederik.prijck@auth0.com>
2021-08-23 12:35:26 +0200

    [SDK-2673] Run tests against Node OIDC on BrowserStack (#457)

--- cypress/integration/smoke.test.ts
+++ cypress/integration/smoke.test.ts
@@ -1,17 +1,32 @@
-const EMAIL = Cypress.env('USER_EMAIL');
-const PASSWORD = Cypress.env('USER_PASSWORD');
+const useAuth0 = Cypress.env('USE_AUTH0');
+const EMAIL = useAuth0 ? Cypress.env('USER_EMAIL') : 'test';
+const PASSWORD = useAuth0 ? Cypress.env('USER_PASSWORD') : 'test';
 
 if (!EMAIL || !PASSWORD) {
   throw new Error('You must provide CYPRESS_USER_EMAIL and CYPRESS_USER_PASSWORD environment variables');
 }
 
+const loginToAuth0 = () => {
+  cy.get('input[name=email], input[name=username]').focus().clear().type(EMAIL);
+  cy.get('input[name=password]').focus().clear().type(PASSWORD);
+  cy.get('button[name=submit], button[name=action]').click();
+};
+
+const loginToNodeOidc = () => {
+  cy.get('input[name=login]').focus().clear().type(EMAIL);
+  cy.get('input[name=password]').focus().clear().type(PASSWORD);
+  cy.get('button.login').click();
+  cy.get('button.login').click();
+};
+
+const login = useAuth0 ? loginToAuth0 : loginToNodeOidc;
+
 describe('smoke tests', () => {
   before(() => {
     cy.visit('/');
+    cy.window().its('__DEV_PAGES_MANIFEST'); // wait for pages to load so FF doesn't fail
     cy.get('[data-testid=login]').click();
-    cy.get('input[name=email], input[name=username]').focus().clear().type(EMAIL);
-    cy.get('input[name=password]').focus().clear().type(PASSWORD);
-    cy.get('button[name=submit], button[name=action]').click();
+    login();
   });
 
   it('should do basic login and show user', () => {
@@ -34,6 +49,9 @@ describe('smoke tests', () => {
 
   it('should logout and return to the index page', () => {
     cy.get('[data-testid=logout]').click();
+    if (!useAuth0) {
+      cy.get('[name=logout]').click();
+    }
     cy.url().should('eq', `${Cypress.config().baseUrl}/`);
     cy.get('[data-testid=login]').should('exist');
   });
