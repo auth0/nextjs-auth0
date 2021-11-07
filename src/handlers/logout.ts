cbd452782e47c9fce25f94f24c3a69418f48bc12
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-07 13:59:03 +0900

    Allow logout API to accept returnTo query string

--- src/handlers/logout.ts
+++ src/handlers/logout.ts
@@ -2,6 +2,7 @@ import { NextApiResponse, NextApiRequest } from 'next';
 import { HandleLogout as BaseHandleLogout } from '../auth0-session';
 import { assertReqRes } from '../utils/assert';
 import { HandlerError } from '../utils/errors';
+import isSafeRedirect from '../utils/url-helpers';
 
 /**
  * Custom options to pass to logout.
@@ -32,6 +33,16 @@ export default function handleLogoutFactory(handler: BaseHandleLogout): HandleLo
   return async (req, res, options): Promise<void> => {
     try {
       assertReqRes(req, res);
+      if (req.query.returnTo) {
+        const returnTo = Array.isArray(req.query.returnTo) ? req.query.returnTo[0] : req.query.returnTo;
+
+        if (!isSafeRedirect(returnTo)) {
+          throw new Error('Invalid value provided for returnTo, must be a relative url');
+        }
+
+        options = { ...options, returnTo };
+      }
+
       return await handler(req, res, options);
     } catch (e) {
       throw new HandlerError(e);
