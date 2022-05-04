054d2b2896ddfe657f829510a1f2e1b3eb5ce11a
adamjmcgrath <adam.mcgrath@auth0.com>
2022-05-04 15:51:47 +0100

    Fix tests

--- tests/fixtures/test-app/pages/api/session.ts
+++ tests/fixtures/test-app/pages/api/session.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function sessionHandler(req: NextApiRequest, res: NextApiResponse): void {
-  const json = JSON.stringify((global as any).getSession(req, res));
+  const json = (global as any).getSession(req, res);
   res.status(200).json(json);
 }
