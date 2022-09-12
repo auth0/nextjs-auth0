80372ed46ab263ae30e72401edb89be8a10fe752
adamjmcgrath <adam.mcgrath@auth0.com>
2022-09-12 11:11:46 +0100

    Update SessionCache types, Fix doc comments

--- src/auth0-session/session-cache.ts
+++ src/auth0-session/session-cache.ts
@@ -1,9 +1,10 @@
 import type { TokenSet } from 'openid-client';
+import type { IncomingMessage, ServerResponse } from 'http';
 
-export interface SessionCache {
-  create(req: unknown, res: unknown, session: { [key: string]: any }): Promise<void>;
-  delete(req: unknown, res: unknown): Promise<void>;
-  isAuthenticated(req: unknown, res: unknown): Promise<boolean>;
-  getIdToken(req: unknown, res: unknown): Promise<string | undefined>;
-  fromTokenSet(tokenSet: TokenSet): { [key: string]: any };
+export interface SessionCache<Req = IncomingMessage, Res = ServerResponse, Session = { [key: string]: any }> {
+  create(req: Req, res: Res, session: Session): Promise<void>;
+  delete(req: Req, res: Res): Promise<void>;
+  isAuthenticated(req: Req, res: Res): Promise<boolean>;
+  getIdToken(req: Req, res: Res): Promise<string | undefined>;
+  fromTokenSet(tokenSet: TokenSet): Session;
 }
