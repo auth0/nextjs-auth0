71ea39678b3d94e94eb62afd27f0000bbc0501f4
Rita Zerrizuela <zeta@widcket.com>
2022-09-22 03:12:28 -0300

    Revert test changes

--- examples/kitchen-sink-example/pages/api/auth/[auth0].ts
+++ examples/kitchen-sink-example/pages/api/auth/[auth0].ts
@@ -1,27 +1,6 @@
 import { handleAuth } from '@auth0/nextjs-auth0';
 
 export default handleAuth({
-  login: {
-    authorizationParams: { scope: 'openid email offline_access' },
-    getLoginState() {
-      return { foo: 'bar' };
-    }
-  },
-  logout: { returnTo: 'https://example.com/foo' },
-  callback: {
-    async afterCallback(req, res, session) {
-      console.log('After callback!!');
-      return session;
-    },
-    authorizationParams: { scope: 'openid email' }
-  },
-  profile: {
-    refetch: true,
-    async afterRefetch(req, res, session) {
-      console.log('After refetch!!');
-      return session;
-    }
-  },
   onError(req, res, error) {
     console.error(error);
     res.status(error.status || 500).end('Check the console for the error');
