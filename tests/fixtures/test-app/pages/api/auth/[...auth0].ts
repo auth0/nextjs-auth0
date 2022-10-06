708f039e73f7c60abe40fa322e9e7af36b3d89bc
Rita Zerrizuela <zeta@widcket.com>
2022-10-05 22:00:26 -0300

    Add missing types in test app

--- tests/fixtures/test-app/pages/api/auth/[...auth0].ts
+++ tests/fixtures/test-app/pages/api/auth/[...auth0].ts
@@ -1,1 +1,2 @@
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default (...args: any) => global.handleAuth?.()(...args);
--- tests/fixtures/test-app/pages/api/update-user.ts
+++ tests/fixtures/test-app/pages/api/update-user.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function sessionHandler(req: NextApiRequest, res: NextApiResponse) {
+export default async function sessionHandler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   const session = await global.getSession?.(req, res);
   const updated = { ...session?.user, ...req.body?.user };
   await global.updateUser?.(req, res, updated);
