f478770e18717781c05b8c775c20de7ef36b9e96
adamjmcgrath <adam.mcgrath@auth0.com>
2022-10-07 08:42:32 +0100

    Release v1.9.2

---
+++ docs/assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = JSON.parse("{\"kinds\":{},\"rows\":[],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"parent\"],\"fieldVectors\":[],\"invertedIndex\":[],\"pipeline\":[]}}");
