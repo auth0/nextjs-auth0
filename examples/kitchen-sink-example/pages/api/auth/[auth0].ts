5621e37b9f598137377fb66d5e7303ef3872c8e8
adamjmcgrath <adam.mcgrath@auth0.com>
2023-01-17 11:53:53 +0000

    Test chunked cookies and middleware

--- examples/kitchen-sink-example/pages/api/auth/[auth0].ts
+++ examples/kitchen-sink-example/pages/api/auth/[auth0].ts
@@ -1,6 +1,13 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { randomBytes } from 'crypto';
+import { handleAuth, handleCallback } from '@auth0/nextjs-auth0';
 
 export default handleAuth({
+  callback: handleCallback({
+    afterCallback(req, res, session, state) {
+      session.user.customProp = randomBytes(2000).toString('base64');
+      return session;
+    }
+  }),
   onError(req, res, error) {
     console.error(error);
     res.status(error.status || 500).end('Check the console for the error');
