24b2bb9da2d74a3039b729692742d1432c391fe3
adamjmcgrath <adam.mcgrath@auth0.com>
2023-01-23 12:51:28 +0000

    Fix test build

--- tests/fixtures/global.d.ts
+++ tests/fixtures/global.d.ts
@@ -1,17 +1,13 @@
 declare global {
-  namespace NodeJS {
-    interface Global {
-      getSession?: Function;
-      updateSession?: Function;
-      handleAuth?: Function;
-      withApiAuthRequired?: Function;
-      withPageAuthRequired?: Function;
-      withPageAuthRequiredCSR?: Function;
-      getAccessToken?: Function;
-      asyncProps?: boolean;
-      onError?: Function;
-    }
-  }
+  var getSession: Function | undefined;
+  var updateSession: Function | undefined;
+  var handleAuth: Function | undefined;
+  var withApiAuthRequired: Function | undefined;
+  var withPageAuthRequired: Function | undefined;
+  var withPageAuthRequiredCSR: Function | undefined;
+  var getAccessToken: Function | undefined;
+  var asyncProps: boolean | undefined;
+  var onError: Function | undefined;
 }
 
 export {};
