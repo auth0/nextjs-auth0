c59cbcc5e14f0ccd8b42318093fe9b2af0f9f7a1
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-25 16:46:44 +0100

    Add profile refetch to app dir handler

--- example-app/app/api/auth/[auth0]/route.ts
+++ example-app/app/api/auth/[auth0]/route.ts
@@ -1,6 +1,7 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleProfile } from '@auth0/nextjs-auth0';
 
 export const GET = handleAuth({
+  profile: handleProfile({ refetch: true }),
   onError(req: Request, error: Error) {
     console.error(error);
   }
