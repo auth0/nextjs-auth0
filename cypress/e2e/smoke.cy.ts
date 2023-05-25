f1a98ec6df7c91d47d92c357a9c13da80817bf93
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-25 13:40:58 +0100

    Add smoke tests for app dir

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -21,46 +21,57 @@ const loginToNodeOidc = () => {
 
 const login = useAuth0 ? loginToAuth0 : loginToNodeOidc;
 
-describe('smoke tests page-router', () => {
-  it('should do basic login and show user', () => {
-    cy.visit('/page-router');
-    cy.get('[data-testid=login]').click();
-    login();
-    cy.url().should('eq', `${Cypress.config().baseUrl}/page-router`);
-    cy.get('[data-testid=logout]').should('exist');
+describe('smoke tests', () => {
+  describe('page router', () => {
+    it('should do basic login and show user', () => {
+      cy.visit('/page-router');
+      cy.get('[data-testid=login]').click();
+      login();
+      cy.url().should('eq', `${Cypress.config().baseUrl}/page-router`);
+      cy.get('[data-testid=logout]').should('exist');
+    });
+
+    it('should protect a client-side rendered page', () => {
+      cy.visit('/page-router/profile-csr');
+      login();
+      cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-csr`);
+      cy.get('[data-testid=profile]').contains(EMAIL);
+    });
+
+    it('should protect a server-side rendered page', () => {
+      cy.visit('/page-router/profile-ssr');
+      login();
+
+      cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-ssr`);
+      cy.get('[data-testid=profile]').contains(EMAIL);
+    });
+
+    it('should protect a page with middleware', () => {
+      cy.visit('/page-router/profile-middleware');
+      login();
+      cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-middleware`);
+      cy.get('[data-testid=profile]').contains(EMAIL);
+    });
+
+    it('should logout and return to the index page', () => {
+      cy.visit('/page-router/profile-csr');
+      login();
+      cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-csr`);
+      cy.get('[data-testid=logout]').click();
+      if (!useAuth0) {
+        cy.get('[name=logout]').click();
+      }
+      cy.url().should('eq', `${Cypress.config().baseUrl}/page-router`);
+      cy.get('[data-testid=login]').should('exist');
+    });
   });
-
-  it('should protect a client-side rendered page', () => {
-    cy.visit('/page-router/profile-csr');
-    login();
-    cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-csr`);
-    cy.get('[data-testid=profile]').contains(EMAIL);
-  });
-
-  it('should protect a server-side rendered page', () => {
-    cy.visit('/page-router/profile-ssr');
-    login();
-
-    cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-ssr`);
-    cy.get('[data-testid=profile]').contains(EMAIL);
-  });
-
-  it('should protect a page with middleware', () => {
-    cy.visit('/page-router/profile-middleware');
-    login();
-    cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-middleware`);
-    cy.get('[data-testid=profile]').contains(EMAIL);
-  });
-
-  it('should logout and return to the index page', () => {
-    cy.visit('/page-router/profile-csr');
-    login();
-    cy.url().should('eq', `${Cypress.config().baseUrl}/page-router/profile-csr`);
-    cy.get('[data-testid=logout]').click();
-    if (!useAuth0) {
-      cy.get('[name=logout]').click();
-    }
-    cy.url().should('eq', `${Cypress.config().baseUrl}/page-router`);
-    cy.get('[data-testid=login]').should('exist');
+  describe('app router', () => {
+    it('should render an app route', () => {
+      cy.visit('/profile');
+      login();
+      cy.url().should('eq', `${Cypress.config().baseUrl}/profile`);
+      cy.get('[data-testid=server-component]').contains(EMAIL);
+      cy.get('[data-testid=client-component]').contains(EMAIL);
+    });
   });
 });
