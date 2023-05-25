a178663535513e2474a92140861704c9800346d6
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-25 13:15:28 +0100

    Add Server and Client components

--- src/http/auth0-next-response-cookies.ts
+++ src/http/auth0-next-response-cookies.ts
@@ -9,11 +9,16 @@ export default class Auth0NextResponseCookies extends Auth0ResponseCookies {
 
   public setCookie(name: string, value: string, options?: CookieSerializeOptions) {
     const cookieSetter = cookies();
-    cookieSetter.set({ ...options, name, value });
+    try {
+      cookieSetter.set({ ...options, name, value });
+    } catch (_) {
+      console.warn('cant set cookies in app dir pages or server components');
+    }
   }
 
   public clearCookie(name: string, options?: CookieSerializeOptions) {
     const cookieSetter = cookies();
+    // @ts-ignore see: https://github.com/vercel/next.js/pull/50052
     cookieSetter.delete({ ...options, name, value: '' });
   }
 }
