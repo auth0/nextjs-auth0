6b0da939e5d01fe5e74c35b4c196e1108836fdf9
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-30 14:20:02 +0100

    Simpler app router example

--- example-app/app/api/auth/[auth0]/route.ts
+++ example-app/app/api/auth/[auth0]/route.ts
@@ -1,15 +1,7 @@
 import { handleAuth, handleProfile, Session } from '@auth0/nextjs-auth0';
 import { NextRequest } from 'next/server';
 
-const afterRefetch = (req: NextRequest, session: Session) => {
-  return { ...session, foo: 'bar' };
-};
-
 export const GET = handleAuth({
-  profile: handleProfile({
-    refetch: true,
-    afterRefetch
-  }),
   onError(req: Request, error: Error) {
     console.error(error);
   }
