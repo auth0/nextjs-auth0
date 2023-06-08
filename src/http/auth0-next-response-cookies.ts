9697e8963dd0be9b629b6074c55186429666360c
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-06-08 10:52:41 +0100

    Warn once in development

--- src/http/auth0-next-response-cookies.ts
+++ src/http/auth0-next-response-cookies.ts
@@ -2,6 +2,8 @@ import { cookies } from 'next/headers';
 import type { CookieSerializeOptions } from 'cookie';
 import { Auth0ResponseCookies } from '../auth0-session/http';
 
+let warned = false;
+
 export default class Auth0NextResponseCookies extends Auth0ResponseCookies {
   public constructor() {
     super();
@@ -13,8 +15,15 @@ export default class Auth0NextResponseCookies extends Auth0ResponseCookies {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore see: https://github.com/vercel/next.js/pull/50052
       cookieSetter.set({ ...options, name, value });
+      /* c8 ignore next 6 */
     } catch (_) {
-      console.warn('cant set cookies in app dir pages or server components');
+      if (process.env.NODE_ENV === 'development' && !warned) {
+        console.warn(
+          'nextjs-auth0 is attempting to set cookies from a server component,' +
+            'see https://github.com/auth0/nextjs-auth0/tree/beta#important-limitations-of-the-app-directory'
+        );
+        warned = true;
+      }
     }
   }
 
