7d8f8005b33020d90817de47136696d4074241b5
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-09-11 10:33:57 +0100

    Check handle login

--- example-app/pages/api/page-router-auth/[...auth0].ts
+++ example-app/pages/api/page-router-auth/[...auth0].ts
@@ -1,11 +1,17 @@
 import { pageRouterAuth } from '@/lib/auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const redirectUri = `${process.env.AUTH0_BASE_URL}/api/page-router-auth/callback`;
 
 export default pageRouterAuth.handleAuth({
-  login: pageRouterAuth.handleLogin({
-    authorizationParams: { redirect_uri: redirectUri }
-  }),
+  async login(req: NextApiRequest, res: NextApiResponse) {
+    await pageRouterAuth.handleLogin(req, res, {
+      authorizationParams: {
+        custom_param: 'custom'
+      },
+      returnTo: '/custom-page'
+    });
+  },
   callback: pageRouterAuth.handleCallback({ redirectUri }),
   logout: pageRouterAuth.handleLogout({ returnTo: `${process.env.AUTH0_BASE_URL}/page-router` })
 });
