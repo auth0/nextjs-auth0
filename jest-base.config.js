4eea634c57d203f6ded18012650a916679a39ca2
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-10-12 12:13:58 +0100

    Mock Next.js in page router tests

--- jest-base.config.js
+++ jest-base.config.js
@@ -3,7 +3,6 @@ module.exports = {
   rootDir: '.',
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   preset: 'ts-jest/presets/js-with-ts',
-  globalSetup: './tests/global-setup.ts',
   setupFilesAfterEnv: ['./tests/setup.ts'],
   transformIgnorePatterns: ['/node_modules/(?!oauth4webapi)']
 };
--- tests/fixtures/test-app/pages/csr-protected.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NextPageContext } from 'next';
-
-// eslint-disable-next-line react/prop-types
-export default function protectedPage(): React.ReactElement {
-  return global.withPageAuthRequiredCSR?.(() => <div>Protected Page</div>);
-}
-
-export const getServerSideProps = (ctx: NextPageContext): any => global.withPageAuthRequired?.()(ctx);
