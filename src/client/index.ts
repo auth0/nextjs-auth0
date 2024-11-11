46e30adadc20f4efd1e2f1016f206036d05ed0cc
guabu <135956181+guabu@users.noreply.github.com>
2024-11-11 13:03:41 +0100

    fix: ESM imports for Pages router (#1798)

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,2 +1,2 @@
-export { useUser } from "./hooks/use-user"
-export { getAccessToken } from "./helpers/get-access-token"
+export { useUser } from "./hooks/use-user.js"
+export { getAccessToken } from "./helpers/get-access-token.js"
