dea232d213b05e500d00105937ed16901337161e
guabu <135956181+guabu@users.noreply.github.com>
2024-11-23 20:58:04 +0100

    fix: prevent revalidation during error

--- src/client/hooks/use-user.ts
+++ src/client/hooks/use-user.ts
@@ -5,10 +5,25 @@ import useSWR from "swr"
 import { User } from "../../server/user"
 
 export function useUser() {
-  const { data, error, isLoading } = useSWR<User, {}, string>(
+  const { data, error, isLoading } = useSWR<User, Error, string>(
     "/auth/profile",
-    (...args) => fetch(...args).then((res) => res.json())
+    (...args) =>
+      fetch(...args).then((res) => {
+        if (!res.ok) {
+          throw new Error("Unauthorized")
+        }
+
+        return res.json()
+      })
   )
+
+  if (error) {
+    return {
+      user: null,
+      isLoading: false,
+      error,
+    }
+  }
 
   return {
     user: data,
