0c7b5c60896d01cd6e5b918737723aa5f07d17c0
guabu <135956181+guabu@users.noreply.github.com>
2024-11-24 13:10:23 +0100

    chore: fix return

--- e2e/test-app/lib/auth0.ts
+++ e2e/test-app/lib/auth0.ts
@@ -1,3 +1,7 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server"
 
-export const auth0 = new Auth0Client()
+export const auth0 = new Auth0Client({
+  authorizationParameters: {
+    audience: "https://example.com/test-api",
+  },
+})
