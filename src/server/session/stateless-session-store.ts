0f7b74e313f17f899b969893d0b82f427f95c42a
guabu <135956181+guabu@users.noreply.github.com>
2024-11-24 13:38:38 +0100

    chore: add warning when cookie size exceeds 4096 bytes

--- src/server/session/stateless-session-store.ts
+++ src/server/session/stateless-session-store.ts
@@ -51,6 +51,20 @@ export class StatelessSessionStore extends AbstractSessionStore {
       ...this.cookieConfig,
       maxAge,
     })
+
+    // check if the session cookie size exceeds 4096 bytes, and if so, log a warning
+    const cookieJarSizeTest = new cookies.ResponseCookies(new Headers())
+    cookieJarSizeTest.set(this.SESSION_COOKIE_NAME, jwe.toString(), {
+      ...this.cookieConfig,
+      maxAge,
+    })
+    if (new TextEncoder().encode(cookieJarSizeTest.toString()).length >= 4096) {
+      console.warn(
+        "The session cookie size exceeds 4096 bytes, which may cause issues in some browsers. " +
+          "Consider removing any unnecessary custom claims from the access token or the user profile. " +
+          "Alternatively, you can use a stateful session implementation to store the session data in a data store."
+      )
+    }
   }
 
   async delete(
