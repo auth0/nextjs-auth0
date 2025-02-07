4d398b24fb48e9d752607e1065b042ccd91c4a4e
guabu <135956181+guabu@users.noreply.github.com>
2025-02-07 08:46:29 +0100

    chore: clean slate for v4

--- src/auth0-session/session-cache.ts
+++
@@ -1,9 +0,0 @@
-import { TokenEndpointResponse } from './client/abstract-client';
-
-export interface SessionCache<Req = any, Res = any, Session = { [key: string]: any }> {
-  create(req: Req, res: Res, session: Session): Promise<void>;
-  delete(req: Req, res: Res): Promise<void>;
-  isAuthenticated(req: Req, res: Res): Promise<boolean>;
-  getIdToken(req: Req, res: Res): Promise<string | undefined>;
-  fromTokenEndpointResponse(req: Req, res: Res, tokenSet: TokenEndpointResponse): Promise<Session>;
-}
