4687afb1cc8229f15e88b26b6c4a8877702c60c1
guabu <135956181+guabu@users.noreply.github.com>
2025-02-07 08:46:29 +0100

    chore: clean slate for v4

--- jest-base.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  rootDir: '.',
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
-  preset: 'ts-jest/presets/js-with-ts',
-  setupFilesAfterEnv: ['./tests/setup.ts'],
-  transformIgnorePatterns: ['/node_modules/(?!oauth4webapi)']
-};
--- src/auth0-session/utils/debug.ts
+++
@@ -1,3 +0,0 @@
-import createDebug, { Debugger } from 'debug';
-
-export default (name: string): Debugger => createDebug('nextjs-auth0').extend(name);
