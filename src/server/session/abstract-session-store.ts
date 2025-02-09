9743f8e595dd68fc13fd08b91d5e3d48e460e58f
Tushar Pandey <tushar.pandey@okta.com>
2025-02-10 01:18:54 +0530

    remove extra docs from abstract session store

--- src/server/session/abstract-session-store.ts
+++ src/server/session/abstract-session-store.ts
@@ -1,8 +1,6 @@
 import type { SessionData, SessionDataStore } from "../../types"
 import {
   CookieOptions,
-  decrypt,
-  encrypt,
   ReadonlyRequestCookies,
   RequestCookies,
   ResponseCookies,
@@ -71,98 +69,51 @@ interface SessionStoreOptions extends SessionConfiguration {
 
 const SESSION_COOKIE_NAME = "__session"
 
-/**
- * AbstractSessionStore serves as an abstract base class for managing session storage.
- * It defines essential properties and methods that any session store implementation must provide.
- */
 export abstract class AbstractSessionStore {
-  /**
-   * A secret key used for encrypting session data.
-   */
   public secret: string
-
-  /**
-   * The name of the cookie used to store the session identifier.
-   */
   public sessionCookieName: string
 
-  /**
-   * Indicates whether the session should be refreshed on each request.
-   * Default is true, meaning the session will be extended with each request.
-   */
-  private readonly rolling: boolean
+  private rolling: boolean
+  private absoluteDuration: number
+  private inactivityDuration: number
 
-  /**
-   * The maximum duration (in seconds) a session can last, regardless of activity.
-   * Default is 3 days (259200 seconds).
-   */
-  private readonly absoluteDuration: number
-
-  /**
-   * The duration (in seconds) after which a session will expire if there is no activity.
-   * Default is 1 day (86400 seconds).
-   */
-  private readonly inactivityDuration: number
-
-  /**
-   * An optional reference to a specific session data store implementation.
-   */
   public store?: SessionDataStore
 
-  /**
-   * Configuration options for the session cookie, including security settings.
-   */
   public cookieConfig: CookieOptions
 
-  /**
-   * Constructor to initialize an instance of AbstractSessionStore.
-   *
-   * @param {SessionStoreOptions} options - Configuration options for the session store.
-   */
   constructor({
     secret,
+
     rolling = true,
-    absoluteDuration = 60 * 60 * 24 * 3, // Default: 3 days in seconds
-    inactivityDuration = 60 * 60 * 24 * 1, // Default: 1 day in seconds
+    absoluteDuration = 60 * 60 * 24 * 3, // 3 days in seconds
+    inactivityDuration = 60 * 60 * 24 * 1, // 1 day in seconds
     store,
+
     cookieOptions,
   }: SessionStoreOptions) {
-    this.secret = secret // Set the secret key for encryption
+    this.secret = secret
 
-    this.rolling = rolling // Set whether the session is rolling
-    this.absoluteDuration = absoluteDuration // Set absolute duration
-    this.inactivityDuration = inactivityDuration // Set inactivity duration
-    this.store = store // Set the optional session data store
+    this.rolling = rolling
+    this.absoluteDuration = absoluteDuration
+    this.inactivityDuration = inactivityDuration
+    this.store = store
 
-    // Set up cookie configuration with defaults and provided options
     this.sessionCookieName = cookieOptions?.name ?? SESSION_COOKIE_NAME
     this.cookieConfig = {
-      httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
-      sameSite: cookieOptions?.sameSite ?? "lax", // CSRF protection setting
-      secure: cookieOptions?.secure ?? false, // Use secure cookies if true (HTTPS)
-      path: "/", // Cookie is valid for all paths
+      httpOnly: true,
+      sameSite: cookieOptions?.sameSite ?? "lax",
+      secure: cookieOptions?.secure ?? false,
+      path: "/",
     }
   }
 
-  /**
-   * Retrieves the session data based on the provided request cookies.
-   *
-   * @param {RequestCookies | ReadonlyRequestCookies} reqCookies - Cookies from the request.
-   * @returns {Promise<SessionData | null>} - A promise that resolves to the session data or null if no session exists.
-   */
   abstract get(
     reqCookies: RequestCookies | ReadonlyRequestCookies
   ): Promise<SessionData | null>
 
   /**
-   * Saves the encrypted session cookie as a `Set-Cookie` header in the response.
-   * If the `iat` (issued at) property is present in the session, it will be used to compute the `maxAge` cookie value.
-   *
-   * @param {RequestCookies | ReadonlyRequestCookies} reqCookies - Cookies from the request.
-   * @param {ResponseCookies} resCookies - Cookies to be sent in the response.
-   * @param {SessionData} session - The session data to be saved.
-   * @param {boolean} [isNew=false] - Indicates if this is a new session. Defaults to false.
-   * @returns {Promise<void>} - A promise that resolves when the operation is complete.
+   * save adds the encrypted session cookie as a `Set-Cookie` header. If the `iat` property
+   * is present on the session, then it will be used to compute the `maxAge` cookie value.
    */
   abstract set(
     reqCookies: RequestCookies | ReadonlyRequestCookies,
@@ -171,47 +122,33 @@ export abstract class AbstractSessionStore {
     isNew?: boolean
   ): Promise<void>
 
-  /**
-   * Deletes the session based on the provided request cookies and updates response cookies accordingly.
-   *
-   * @param {RequestCookies | ReadonlyRequestCookies} reqCookies - Cookies from the request.
-   * @param {ResponseCookies} resCookies - Cookies to be sent in the response after deletion.
-   * @returns {Promise<void>} - A promise that resolves when the operation is complete.
-   */
   abstract delete(
     reqCookies: RequestCookies | ReadonlyRequestCookies,
     resCookies: ResponseCookies
   ): Promise<void>
 
   /**
-   * Returns the current time in seconds since the Unix epoch (January 1, 1970).
-   *
-   * @returns {number} - The current time in seconds since epoch.
+   * epoch returns the time since unix epoch in seconds.
    */
   epoch() {
-    return (Date.now() / 1000) | 0 // Convert milliseconds to seconds and floor it to an integer
+    return (Date.now() / 1000) | 0
   }
 
   /**
-   * Calculates the maximum age of the session based on its creation time,
-   * considering both rolling and absolute durations.
-   *
-   * @param {number} createdAt - The timestamp when the session was created (in seconds).
-   * @returns {number} - The maximum age of the session in seconds or zero if expired.
+   * calculateMaxAge calculates the max age of the session based on createdAt and the rolling and absolute durations.
    */
   calculateMaxAge(createdAt: number) {
     if (!this.rolling) {
-      return this.absoluteDuration // Return absolute duration if not rolling
+      return this.absoluteDuration
     }
 
-    const updatedAt = this.epoch() // Get current time
+    const updatedAt = this.epoch()
     const expiresAt = Math.min(
-      updatedAt + this.inactivityDuration, // Time after inactivity duration
-      createdAt + this.absoluteDuration // Time after absolute duration
+      updatedAt + this.inactivityDuration,
+      createdAt + this.absoluteDuration
     )
+    const maxAge = expiresAt - this.epoch()
 
-    const maxAge = expiresAt - this.epoch() // Calculate max age based on expiration time
-
-    return maxAge > 0 ? maxAge : 0 // Return max age or zero if expired
+    return maxAge > 0 ? maxAge : 0
   }
 }
