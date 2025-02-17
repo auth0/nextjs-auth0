39787502909b1e338918920b1ffc126816349b4b
Frederik Prijck <frederik.prijck@auth0.com>
2025-02-17 09:01:09 +0100

    Add support for Federated Connection Access Token (#1911) (#1922)
    Co-authored-by: Tushar Pandey <tushar.pandey@okta.com>

--- src/server/session/stateless-session-store.ts
+++ src/server/session/stateless-session-store.ts
@@ -1,4 +1,7 @@
-import { CookieOptions, SessionData } from "../../types";
+import { CookieOptions, FederatedConnectionTokenSet, SessionData } from "../../types";
+
+import type { JWTPayload } from "jose";
+
 import * as cookies from "../cookies";
 import {
   AbstractSessionStore,
@@ -21,6 +24,8 @@ interface StatelessSessionStoreOptions {
 }
 
 export class StatelessSessionStore extends AbstractSessionStore {
+  federatedConnectionTokenSetsCookieName = "__FC";
+
   constructor({
     secret,
     rolling,
@@ -50,7 +55,27 @@ export class StatelessSessionStore extends AbstractSessionStore {
       SessionData | LegacySessionPayload
     >(cookieValue, this.secret);
 
-    return normalizeStatelessSession(originalSession);
+    const normalizedStatelessSession = normalizeStatelessSession(originalSession);
+
+    // As federated connection access tokens are stored in seperate cookies,
+    // we need to get all cookies and only use those that are prefixed with `this.federatedConnectionTokenSetsCookieName`
+    const federatedConnectionTokenSets = await Promise.all(
+      this.getFederatedConnectionTokenSetsCookies(reqCookies).map(
+        (fcatCookie) =>
+          cookies.decrypt<FederatedConnectionTokenSet>(
+            fcatCookie.value,
+            this.secret
+          )
+      )
+    );
+
+    return {
+      ...normalizedStatelessSession,
+      // Ensure that when there are no federated connection token sets, we omit the property.
+      ...(federatedConnectionTokenSets.length
+        ? { federatedConnectionTokenSets: federatedConnectionTokenSets.map(tokenSet => tokenSet.payload) }
+        : {})
+    };
   }
 
   /**
@@ -61,7 +86,8 @@ export class StatelessSessionStore extends AbstractSessionStore {
     resCookies: cookies.ResponseCookies,
     session: SessionData
   ) {
-    const jwe = await cookies.encrypt(session, this.secret);
+    const { federatedConnectionTokenSets, ...originalSession } = session;
+    const jwe = await cookies.encrypt(originalSession, this.secret);
     const maxAge = this.calculateMaxAge(session.internal.createdAt);
     const cookieValue = jwe.toString();
     const options: CookieOptions = {
@@ -76,16 +102,88 @@ export class StatelessSessionStore extends AbstractSessionStore {
       reqCookies,
       resCookies
     );
+
+
+    // Store federated connection access tokens, each in its own cookie
+    if (federatedConnectionTokenSets?.length) {
+      await Promise.all(
+        federatedConnectionTokenSets.map((federatedConnectionTokenSet, index) =>
+          this.storeInCookie(
+            reqCookies,
+            resCookies,
+            federatedConnectionTokenSet,
+            `${this.federatedConnectionTokenSetsCookieName}_${index}`,
+            maxAge
+          )
+        )
+      );
+    }
   }
 
   async delete(
-    _reqCookies: cookies.RequestCookies,
+    reqCookies: cookies.RequestCookies,
     resCookies: cookies.ResponseCookies
   ) {
     cookies.deleteChunkedCookie(
       this.sessionCookieName,
-      _reqCookies,
+      reqCookies,
       resCookies
     );
+    this.getFederatedConnectionTokenSetsCookies(reqCookies).forEach((cookie) =>
+      resCookies.delete(cookie.name)
+    );
+  }
+
+  private async storeInCookie(
+    reqCookies: cookies.RequestCookies,
+    resCookies: cookies.ResponseCookies,
+    session: JWTPayload,
+    cookieName: string,
+    maxAge: number
+  ) {
+    const jwe = await cookies.encrypt(session, this.secret);
+
+    const cookieValue = jwe.toString();
+
+    resCookies.set(cookieName, jwe.toString(), {
+      ...this.cookieConfig,
+      maxAge
+    });
+    // to enable read-after-write in the same request for middleware
+    reqCookies.set(cookieName, cookieValue);
+
+    // check if the session cookie size exceeds 4096 bytes, and if so, log a warning
+    const cookieJarSizeTest = new cookies.ResponseCookies(new Headers());
+    cookieJarSizeTest.set(cookieName, cookieValue, {
+      ...this.cookieConfig,
+      maxAge
+    });
+
+    if (new TextEncoder().encode(cookieJarSizeTest.toString()).length >= 4096) {
+      // if the cookie is the session cookie, log a warning with additional information about the claims and user profile.
+      if (cookieName === this.sessionCookieName) {
+        console.warn(
+          `The ${cookieName} cookie size exceeds 4096 bytes, which may cause issues in some browsers. ` +
+            "Consider removing any unnecessary custom claims from the access token or the user profile. " +
+            "Alternatively, you can use a stateful session implementation to store the session data in a data store."
+        );
+      } else {
+        console.warn(
+          `The ${cookieName} cookie size exceeds 4096 bytes, which may cause issues in some browsers. ` +
+            "You can use a stateful session implementation to store the session data in a data store."
+        );
+      }
+      
+    }
+  }
+
+  private getFederatedConnectionTokenSetsCookies(
+    cookies: cookies.RequestCookies | cookies.ResponseCookies
+  ) {
+    return cookies
+      .getAll()
+      .filter((cookie) =>
+        cookie.name.startsWith(this.federatedConnectionTokenSetsCookieName)
+      );
   }
 }
