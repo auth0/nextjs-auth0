e639a877e5b3f705903f58124c5b80854ba5195c
kenkoooo <kenkou.n@gmail.com>
2025-02-20 14:54:24 +0900

    feat: add idToken support in AuthClient

--- src/server/auth-client.ts
+++ src/server/auth-client.ts
@@ -476,6 +476,7 @@ export class AuthClient {
       user: idTokenClaims,
       tokenSet: {
         accessToken: oidcRes.access_token,
+        idToken: oidcRes.id_token,
         scope: oidcRes.scope,
         refreshToken: oidcRes.refresh_token,
         expiresAt: Math.floor(Date.now() / 1000) + Number(oidcRes.expires_in)
@@ -551,7 +552,6 @@ export class AuthClient {
         }
       );
     }
-
     const res = NextResponse.json({
       token: updatedTokenSet.accessToken,
       scope: updatedTokenSet.scope,
@@ -670,6 +670,7 @@ export class AuthClient {
       const updatedTokenSet = {
         ...tokenSet, // contains the existing `iat` claim to maintain the session lifetime
         accessToken: oauthRes.access_token,
+        idToken: oauthRes.id_token,
         expiresAt: accessTokenExpiresAt
       };
 
