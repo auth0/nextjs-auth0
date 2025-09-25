b9b0d1fe5e0199f8ff5790f7b52e7790a69171c2
Frederik Prijck <frederik.prijck@okta.com>
2025-09-25 12:59:32 +0200

    Simplify MRRT sample

--- examples/with-mrrt/app/page.tsx
+++ examples/with-mrrt/app/page.tsx
@@ -25,7 +25,7 @@ export default function Page() {
   async function getToken(e: ReactMouseEvent<HTMLButtonElement, MouseEvent>) {
     "use server";
 
-    const token = await auth0.getAccessToken({ refresh: true });
+    const token = await auth0.getAccessToken();
 
     // Logging the token on the server-side console to allow you to inspect it.
     console.log(token);
@@ -56,21 +56,6 @@ export default function Page() {
     "use server";
 
     const token = await auth0.getAccessToken({ audience: "{YOUR_SECOND_API_IDENTIFIER_HERE}" });
-
-    // Logging the token on the server-side console to allow you to inspect it.
-    console.log(token);
-  }
-
-  /**
-   * Get an access token for audience 'test-3'.
-   * @param e - React mouse event
-   */
-  async function getTokenForAudience3(
-    e: ReactMouseEvent<HTMLButtonElement, MouseEvent>
-  ) {
-    "use server";
-
-    const token = await auth0.getAccessToken({ audience: "{YOUR_THIRD_API_IDENTIFIER_HERE}" });
 
     // Logging the token on the server-side console to allow you to inspect it.
     console.log(token);
@@ -182,14 +167,6 @@ export default function Page() {
                 onClick={getTokenForAudience2}
               >
                 Get Token (Audience 2)
-              </button>
-
-              <button
-                type="button"
-                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-                onClick={getTokenForAudience3}
-              >
-                Get Token (Audience 3)
               </button>
             </div>
             <div className="aspect-video rounded-xl bg-muted/50 p-4">
