a22df3b28e9c32b68569c9533b2e1b58f07c4216
Frederik Prijck <frederik.prijck@okta.com>
2025-11-04 17:53:29 +0100

    remove function from errors.ts as that is exported

--- src/server/auth-client.ts
+++ src/server/auth-client.ts
@@ -15,7 +15,6 @@ import {
   BackchannelAuthenticationError,
   BackchannelAuthenticationNotSupportedError,
   BackchannelLogoutError,
-  buildConnectAccountErrorResponse,
   ConnectAccountError,
   ConnectAccountErrorCodes,
   DiscoveryError,
@@ -23,6 +22,7 @@ import {
   DPoPErrorCode,
   InvalidStateError,
   MissingStateError,
+  MyAccountApiError,
   OAuth2Error,
   SdkError
 } from "../errors/index.js";
@@ -2229,3 +2229,45 @@ export type FetcherFactoryOptions<TOutput extends Response> = {
   useDPoP?: boolean;
   getAccessToken: AccessTokenFactory;
 } & FetcherMinimalConfig<TOutput>;
+
+/**
+ * Builds a ConnectAccountError response based on the provided Response object and error code.
+ * @param res The Response object containing the error details.
+ * @param errorCode The ConnectAccountErrorCodes enum value representing the type of error.
+ * @returns
+ */
+export async function buildConnectAccountErrorResponse(
+  res: Response,
+  errorCode: ConnectAccountErrorCodes
+): Promise<[ConnectAccountError, null]> {
+  const actionVerb =
+    errorCode === ConnectAccountErrorCodes.FAILED_TO_INITIATE
+      ? "initiate"
+      : "complete";
+
+  try {
+    const errorBody = await res.json();
+    return [
+      new ConnectAccountError({
+        code: errorCode,
+        message: `The request to ${actionVerb} the connect account flow failed with status ${res.status}.`,
+        cause: new MyAccountApiError({
+          type: errorBody.type,
+          title: errorBody.title,
+          detail: errorBody.detail,
+          status: res.status,
+          validationErrors: errorBody.validation_errors
+        })
+      }),
+      null
+    ];
+  } catch (e) {
+    return [
+      new ConnectAccountError({
+        code: errorCode,
+        message: `The request to ${actionVerb} the connect account flow failed with status ${res.status}.`
+      }),
+      null
+    ];
+  }
+}
