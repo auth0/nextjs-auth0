0468af95e34048dad745578b3dd1305ade783a2c
Frederik Prijck <frederik.prijck@okta.com>
2026-01-12 13:33:23 +0100

    revert unintentional change

--- src/server/auth-client.ts
+++ src/server/auth-client.ts
@@ -446,6 +446,8 @@ export class AuthClient {
       return this.#unwrapHandler(() => this.handleMyOrg(auth0Req, auth0Res));
     } else {
       // no auth handler found, simply touch the sessions
+      // TODO: this should only happen if rolling sessions are enabled. Also, we should
+      // try to avoid reading from the DB (for stateful sessions) on every request if possible.
       const session = await this.sessionStore.get(auth0Req.getCookies());
       const auth0Res = new Auth0NextResponse(NextResponse.next());
       if (session) {
